87b88b037c6927e1f978265c2c6c8461f2938324
AmuroPeng <tiggerpeng1997@gmail.com>
2019-05-11 12:31:05 +0800

    改ui：DCE的图片放不下

--- Draw.py
+++ Draw.py
@@ -10,7 +10,7 @@ def one_line(x, y, title='Curve', save_path='line.jpg'):
     # X轴，Y轴数据
     # x = [0, 1, 2, 3, 4, 5, 6]
     # y = [0.3, 0.4, 2, 5, 3, 4.5, 4]
-    plt.figure(figsize=(6, 4))  # 创建绘图对象
+    plt.figure(figsize=(7, 4))  # 创建绘图对象
     plt.plot(x, y, "b--", linewidth=1)  # 在当前绘图对象绘图（X轴，Y轴，蓝色虚线，线宽度）
     plt.xlabel("Rotation Angle(deg)")  # X轴标签
     plt.ylabel('Deviation(μm)')  # Y轴标签
